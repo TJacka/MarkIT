5a1cb24dc43b8227ee8ed6ffa21d92a206ba0925
TJacka <vegozzy@gmail.com>
2022-09-27 13:20:29 -0700

    Updated functionality and post logo

--- controllers/posts.js
+++ controllers/posts.js
@@ -21,7 +21,7 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      res.render("swipe.ejs", { post: post, user: req.user });
     } catch (err) {
       console.log(err);
     }
